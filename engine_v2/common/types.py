6797ad2cfc4331ebcd2e71221b99eedac99b8339
Yifan Wang <wangjason11@gmail.com>
2025-12-26 10:49:49 -0600

    Week 1 Day 2: make volume optional candle column

--- engine_v2/common/types.py
+++ engine_v2/common/types.py
@@ -14,7 +14,8 @@ COL_L = "l"
 COL_C = "c"
 COL_V = "volume"
 
-REQUIRED_CANDLE_COLS = (COL_TIME, COL_O, COL_H, COL_L, COL_C, COL_V)
+REQUIRED_CANDLE_COLS = (COL_TIME, COL_O, COL_H, COL_L, COL_C)
+OPTIONAL_CANDLE_COLS = (COL_V,)
 
 Direction = Literal[-1, 0, 1]  # -1 bearish, 1 bullish, 0 neutral/unknown
 
